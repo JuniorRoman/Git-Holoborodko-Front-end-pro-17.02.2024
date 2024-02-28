952332e3d740fa704502f699a6e780c7990fb2fe
roman holoborodko <goloborodko.rr@gmail.com>
2024-02-28 22:13:43 +0100

    commit

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-let numOrStr = prompt("input number or string");
-console.log(numOrStr);
+let userYear = prompt("Вкажіть рік народження");
+let city = prompt("Вкажіть місто в якому Ви живете");
+let sport = prompt(
+  "Вкажіть Ваший улюбленний вид спорту з представленних: Шахи, Настільний теніс, Армреслінг"
+);
 
+let cityUa = "України";
+let cityUSA = "США";
+let cityUk = "Великобританії";
+let cap = "Ти живеш у столиці ";
+let cit = "Ти живеш у місті ";
 
-switch (numOrStr && isNaN(+numOrStr)) {
-  case null:
-    console.log("ви скасували");
+let currentYear = new Date().getFullYear();
+let res = currentYear - userYear;
+
+switch (true) {
+  case userYear === null:
+  case userYear.trim() === "":
+    res = "Шкода, що Ви не захотіли ввести свій рік народження ";
     break;
-  case numOrStr.trim():
-    console.log("Empty String");
-    break;
-  case true:
-    console.log("number is Ba_NaN");
-    break;
-  default:
-    console.log("OK!");
+  case userYear != "":
+    res = `Тобі  ${res} р`;
 }
+
+switch (true) {
+  case city === null:
+  case city.trim() === "":
+    city = "Шкода, що Ви не захотіли ввести своє місто";
+    break;
+  case city != "":
+    if (city === "Київ") {
+      city = cap + cityUa;
+    } else if (city === "Вашингтон") {
+      city = cap + cityUSA;
+    } else if (city === "Лондон") {
+      city = cap + cityUk;
+    } else {
+      city = cit + city;
+    }
+}
+
+switch (sport) {
+  case "Шахи":
+    sport = "Круто! Хочеш стати як " + "Магнус Карлсен";
+    break;
+  case "Настільний теніс":
+    sport = "Круто! Хочеш стати як " + "Іван Май";
+    break;
+  case "Армреслінг":
+    sport = "Круто! Хочеш стати як " + "Олег Жох";
+    break;
+}
+
+alert(`${res} \n${city} \n ${sport}`);
