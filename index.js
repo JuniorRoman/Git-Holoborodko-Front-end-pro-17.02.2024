b4110f118bd5a7c43719caab2bce0a51494a4389
roman holoborodko <goloborodko.rr@gmail.com>
2024-03-10 21:07:34 +0100

    com

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 let arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(arr);
-function removeElement(arr, item) {
-  arr = arr.slice();
-  for (let i = 0; i < arr.length; i++) {
-    if (i == item) {
-      arr.splice(item, 1);
-    }
-  }
-  return console.log(arr);
+let array = arr;
+function removeElement(array, item) {
+  array.splice(item - 1, 1);
+  return console.log(array);
 }
 
-removeElement(arr, 3);
-
+removeElement(arr, 2);
+removeElement(arr, 5);
+removeElement(arr, 1);
