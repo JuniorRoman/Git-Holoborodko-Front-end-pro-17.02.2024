723ed0709888dda4b3f0d90de38e06d1b278edfb
roman holoborodko <goloborodko.rr@gmail.com>
2024-03-11 00:17:07 +0100

    commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-
 function removeElement(arr, item) {
-  arr.splice(item - 1, 1);
-  return arr;
+  for (let i = 0; i < arr.length; i++) {
+    if (item <= arr.length) {
+      arr.splice(item - 1, 1);
+      break;
+    } else {
+      return console.log(
+        `Спробуйте ввести меньше число від 1 до ${arr.length}`
+      );
+    }
+  }
+  return console.log(arr);
 }
 
 removeElement(arr, 1);
-console.log(arr);
