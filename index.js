97d4ea7474800a0e5b7338876dad536a14dfc626
roman holoborodko <goloborodko.rr@gmail.com>
2024-03-17 17:27:57 +0100

    commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// Рекурсивне зведення в ступінь
+// Реалізуйте функцію generateKey
 
-function pow(num, degree) {
-  if (degree == 1) {
-    return num;
-  } else {
-    return num * pow(num, degree - 1);
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+let rez = '';
+let n;
+function generateKey(num, char) {
+  for (let i = 0; i < num; i++) {
+    n = Math.ceil(Math.random() * char.length);
+    rez += char.charAt(n);
   }
+  return rez;
 }
-
-console.log(pow(3, 2));
+const key = generateKey(16, characters);
+console.log(key);
